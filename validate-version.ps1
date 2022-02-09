2e27165c5a6149bc63ca7683ca87bc83fd7c8a45
pweiss-mri <pweiss-mri@users.noreply.github.com>
2022-02-09 18:23:05 -0500

    Update validate-version.ps1

--- validate-version.ps1
+++ validate-version.ps1
@@ -1,8 +1,10 @@
+#https://openhackc7p18aw4poi.azurewebsites.net/api/version/poi, $(Build.BuildId)
 Param(
-    [string] [Parameter(Mandatory=$true)] $Uri,
+    [string] [Parameter(Mandatory=$true)] $ApiName,
     [string] [Parameter(Mandatory=$true)] $Version,
+    [boolean] [Parameter(Mandatory=$false)] $Production = $false,
     )
-  $R = Invoke-WebRequest -URI $Uri
+  $R = Invoke-WebRequest -URI "https://openhackc7p18aw4$ApiName$($Production ? "" : "-staging").azurewebsites.net/api/version/$ApiName"
   $reportedVersion = $R.Content
 
   if ($reportedVersion -eq $Version) {
