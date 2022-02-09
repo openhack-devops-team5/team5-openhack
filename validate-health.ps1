abf1695e1ed76b3cc9c9df6d368b026ad42974fe
pweiss-mri <pweiss-mri@users.noreply.github.com>
2022-02-09 18:40:21 -0500

    Update validate-health.ps1

--- validate-health.ps1
+++ validate-health.ps1
@@ -3,11 +3,11 @@ Param(
     [string] [Parameter(Mandatory=$true)] $ApiName,
     [boolean] [Parameter(Mandatory=$false)] $Production = $false
     )
-  $R = Invoke-WebRequest -URI "https://openhackc7p18aw4$ApiName$($Production ? '' : '-staging').azurewebsites.net/api/version/$ApiName"
 
-  if ($R.StatusCode -eq 200) {
-    exit 0;
-  }
+$R = Invoke-WebRequest -URI "https://openhackc7p18aw4$ApiName$($Production ? '' : '-staging').azurewebsites.net/api/version/$ApiName"
 
-  exit 1; #if failed
+if ($R.StatusCode -eq 200) {
+exit 0;
 }
+
+exit 1; #if failed
