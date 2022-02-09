c387d3419c8ae5bdda248c19c3a2af5f49a195f3
pweiss-mri <pweiss-mri@users.noreply.github.com>
2022-02-09 11:32:29 -0500

    Update UtilityTests.cs

--- apis/poi/tests/UnitTests/UtilityTests.cs
+++ apis/poi/tests/UnitTests/UtilityTests.cs
@@ -7,7 +7,7 @@ namespace UnitTests
     {
         [Fact]
         public void TestLoggingEvents()
-        {
+        //{
             Assert.Equal(1000, LoggingEvents.Healthcheck);
             Assert.Equal(2001, LoggingEvents.GetAllPOIs);
             Assert.Equal(2002, LoggingEvents.GetPOIByID);
